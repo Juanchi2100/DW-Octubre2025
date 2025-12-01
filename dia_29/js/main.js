ceca8a84f4d5741fb574403f6d13556bd3bde019
Juanchi2100 <juanchi2100@gmail.com>
2025-12-01 20:41:18 +0100

    dia 29
    nivel 1 y 2

--- dia_29/js/main.js
+++ dia_29/js/main.js
@@ -1,16 +1,18 @@
 //Nivel 1.1 - Creación de los números pares
 
-function npares(){
-    for(let i=1; i <=100; i++){ //recorre todos los números del 1 al 100
+function npares(inicio, fin){
+    for(let i=inicio; i <=fin; i++){ //recorre todos los números del 1 al 100
         if(i % 2 === 0){        // % divide y revisa si hay sobrante en el resultado. Si hay sobrante en la división entre 2, el numero tendrá que ser impar
             console.log(i);     // imprime todos los números pares
         }   
     }
 }
 
-npares(); 
+npares(98,100); 
 
 // Nivel 1.2 - crear un cuadrado de 5x5
+
+console.log("|-------Nivel 1.2-------|");
 
 function cuadradoLleno(){
     for(let fila = 0; fila < 5; fila++){
@@ -24,23 +26,22 @@ function cuadradoLleno(){
 
 cuadradoLleno();
 
-
-console.log("----------");
-
 //nivel 1.3 - crear un cuadrado de 5x5 con el centro vacío
+
+console.log("|-------Nivel 1.3-------|");
 
 function cuadradoVacio(){
     for(let fila = 0; fila < 5; fila++){
         let linea = "";
         for(let col = 0; col < 5; col++){
-            if(fila === 0 || fila === 4){
+            if(fila === 0 || fila === 4){           //condiciones para la primera y última fila que sean completas
                 linea += "* ";
             }
-            else if(col === 0 || col === 4){
+            else if(col === 0 || col === 4){        //condiciones para que la primera y última columna sean llenas
                 linea += "* ";
             }
             else{
-                linea += " ";
+                linea += "  ";                      //el espacio en blanco en el centro, osea, todo lo que no está declarado arriba. Doble espacio para que se alineara bien.
             }
         }
         console.log(linea);
@@ -48,3 +49,34 @@ function cuadradoVacio(){
 }
 
 cuadradoVacio();
+
+//Nivel 2.2 - Saludar
+
+console.log("|-------Nivel 2.2-------|");
+console.log("Es el aviso que aparece al cargar la pagina!");
+
+function saludar(nombre){
+    alert("Hola " + nombre);
+}
+
+function procesarEntradaUsuario(){
+    const nombre = prompt("Cuál es tu nombre?");
+    saludar(nombre);
+}
+
+procesarEntradaUsuario(); 
+
+//Nivel 3.1 - setTimeout
+console.log("|-------Nivel 3.1-------|");
+
+
+function mensaje(){
+    setTimeout(() => {
+        console.log("Mensaje retrasado por 3 segundos.");
+    }, "3000");
+}
+
+mensaje();
+
+//Nivel 3.2 - setInterval
+console.log("|-------Nivel 3.2-------|");

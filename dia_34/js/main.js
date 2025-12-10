03be06f82daf68f41b45da3017c7208809ad7bd4
juanburgospty <juanchi2100@users.noreply.github.com>
2025-12-10 19:00:45 +0100

    dia34 - lightbox y menu

---
+++ dia_34/js/main.js
@@ -0,0 +1,31 @@
+//Nivel 1 LIGHTBOX
+
+const imgs = document.querySelectorAll("gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-image");
+const backdrop = document.getElementById("lightbox-backdrop");
+const closeBtn = document.getElementById("lightbox-close");
+
+backdrop.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+});
+
+imgs.forEach(img => {
+    img.addEventListener("click", (e) => {
+        lightboxImg.src = e.target.src;
+        lightboxImg.alt = e.target.alt;
+        lightbox.classList.add("active");
+    });
+});
+
+[backdrop, closeBtn].forEach(element => {
+    element.addEventListener("click", () => {
+        lightbox.classList.remove("active");
+    });
+});
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "Escape" && lightbox.classList.contains("active")) {
+        lightbox.classList.remove("active");
+    }
+});

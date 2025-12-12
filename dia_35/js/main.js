be37840d0897ab63da6815a5c7ac1a30b60f03e2
Juanchi2100 <juanchi2100@gmail.com>
2025-12-12 13:24:58 +0100

    asincrono4

--- dia_35/js/main.js
+++ dia_35/js/main.js
@@ -3,7 +3,7 @@ new Splide( '.splide' ).mount();
 // Slide Personal
 
 let slideIndex = 1;
-showSlides(SlideIndex);
+showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -15,7 +15,14 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("my-slides");
-    let dots = document.getElementsByClassName("dot");
-    if(n > slides.length)
+    let slides = document.querySelectorAll(".my-slides");
+    let dots = document.querySelectorAll(".dot");
+    if(n > slides.length) {slideIndex = 1}
+    if(n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }

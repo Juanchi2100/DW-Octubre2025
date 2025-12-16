9f93a2a8b5efa36c267c881e7d2328b677714645
Juanchi2100 <juanchi2100@gmail.com>
2025-12-16 13:01:06 +0100

    asincrono 5 - mediaqueries

--- asincrono_5/js/main.js
+++ asincrono_5/js/main.js
@@ -1,21 +1,17 @@
 const containers = document.querySelectorAll(".main__div");
 
 function activado(e){
-    e.target.querySelector(".img").classList.add("activado");
-    containers.forEach((cont) => cont.classList.add("desactivado"));
+    e.target.querySelector(".img").classList.add("active");
+    containers.forEach((container) => container.classList.add("desactivado"));
     e.target.classList.remove("desactivado");
 }
 
 function desactivado(e){
-    e.target.querySelector(".img").classList.remove("activado");
-    e.target.forEach((cont) => cont.classList.remove("desactivado"));
+    e.target.querySelector(".img").classList.remove("active");
+    containers.forEach((container) => container.classList.remove("desactivado"));
 }
 
 containers.forEach((container) => {
     container.addEventListener("mouseenter", activado);
     container.addEventListener("mouseleave", desactivado);
 });
-
-//FALTA
-    //QUE CUANDO MOUSELEAVE --> TODOS REGRESEN A ACTIVO
-    //QUE SEA RESPONSIVE

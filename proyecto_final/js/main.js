63324736c4752ae743cb1912651e5289003f108e
Juanchi2100 <juanchi2100@gmail.com>
2026-01-12 18:32:51 -0500

    proyecto final
    contact & inicio de js

---
+++ proyecto_final/js/main.js
@@ -0,0 +1,65 @@
+
+//Página Principal INDEX
+
+//Copiar email al clipboard desde el icono
+const btnEmail = document.getElementById("btnCopyEmail");
+
+function handlerCopyEmail() {
+    //Obtención el correo del atributo data-email
+    const email = btnEmail.dataset.email;
+
+    // Validación
+    if (!email) return;
+
+    // Evitar que se pulse repetidamente si ya está en estado "copiado"
+    if (btnEmail.classList.contains("copiado")) return;
+
+    // Copiar al portapapeles
+    navigator.clipboard.writeText(email)
+        .then(() => {
+            // Exito. Agregamos la clase.
+            btnEmail.classList.add("copiado");
+
+            //Restaurar el icono original luego de 2.5s
+            setTimeout(() => {
+                btnEmail.classList.remove("copiado");
+            }, 2500);
+        })
+        .catch(err => {console.error("Error al copiar: ", err)});
+};
+
+//EventListener para copiar el correo.
+if (btnEmail) {
+    btnEmail.addEventListener("click", handlerCopyEmail);
+}
+
+//Página de Contacto
+
+//Copiar email al clipboard
+const emailSpan = document.getElementById("emailCopy");
+const emailContainer = document.getElementById("emailCopyContainer")
+
+function handlerCopiarEmail() {
+    //Guardar el correo inicial
+    const email = emailSpan.innerText;
+
+    //Validación.
+    if (email === "¡Copiado!") return;
+
+    //Copiar al clipboard
+    navigator.clipboard.writeText(email)
+        .then(() => {
+            //Exito => se cambia el texto
+            emailSpan.innerText = "¡Copiado!";
+            emailSpan.classList.add("copiado");
+
+            //Restaurar el texto original luego de 2.5s
+            setTimeout(() => {
+                emailSpan.innerText = email;
+                emailSpan.classList.remove("copiado");
+            }, 2500);
+    }).catch(err => {console.error("Error al copiar: ", err)});
+};
+
+//EventListener para copiar el correo.
+emailContainer.addEventListener("click", handlerCopiarEmail);
